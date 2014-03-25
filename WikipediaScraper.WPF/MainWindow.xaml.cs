701b0f888af554f6cbb6c5a6305ff8eb96be7ab2
beritou <beritou@gmail.com>
2014-03-25 14:35:58 -0400

    Reimplemented using async/await.

--- WikipediaScraper.WPF/MainWindow.xaml.cs
+++ WikipediaScraper.WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace WikipediaScraper.WPF
@@ -9,16 +10,29 @@ namespace WikipediaScraper.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string html = String.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
-            string html = String.Empty;
-            using (WebClient client = new WebClient())
-                html = client.DownloadString("http://en.wikipedia.org/wiki/Main_Page");
-            var request = new ScrapeRequest {HTML = html, XPath = "//*[@id=\"mp-dyk\"]/ul"};
+            WebScrapeFromWikipedia();
+        }
+
+        private async void WebScrapeFromWikipedia()
+        {
+            html = await DownloadHTMLFromWikipedia();
+            var request = new ScrapeRequest { HTML = html, XPath = "//*[@id=\"mp-dyk\"]/ul" };
             var scraper = new Scraper();
             string result = scraper.Scrape(request);
             WebBrowser.NavigateToString(result);
+        }
+
+        async Task<string> DownloadHTMLFromWikipedia()
+        {
+            var client = new WebClient();
+            Task<string> downloadStringTask = client.DownloadStringTaskAsync(new Uri("http://en.wikipedia.org/wiki/Main_Page"));
+            string html = await downloadStringTask;
+            return html;
         }
     }
 }
